f0139107bea085428de57a39fcde8386305baf54
Kitpavin <Chaiwong>
2019-09-29 10:48:46 +0700

    add car detail mokup in BasicCarInformationForm.js

--- src/components/CarRegister/BasicCarInformationForm.js
+++ src/components/CarRegister/BasicCarInformationForm.js
@@ -3,11 +3,68 @@ import { Form, Select, Responsive, Input } from 'semantic-ui-react'
 
 function BasicCarInformationForm() {
 
-    const genderOptions = [
-        { key: '1', text: 'One', value: 'three' },
-        { key: '2', text: 'Two', value: 'two' },
-        { key: '3', text: 'Three', value: 'one' },
+    const brandOptions = [
+        {key: '', text: '', value: ''},
+        { key: '1', text: 'Toyata', value: 'Toyata' },
+        { key: '2', text: 'Honda', value: 'Honda' },
+        { key: '3', text: 'Nissan', value: 'Nissan' },
+        { key: '4', text: 'Masda', value: 'Masda' },
+        { key: '5', text: 'Ford', value: 'Ford' },
     ]
+
+    const modelOptions = [
+        {key: '', text: '', value: ''},
+        { key: '1', text: 'Accord', value: 'Accord' },
+        { key: '2', text: 'Brio', value: 'Brio' },
+        { key: '3', text: 'BR-V', value: 'BR-V' },
+        { key: '4', text: 'City', value: 'City' },
+        { key: '5', text: 'Civic', value: 'Civic' },
+        { key: '5', text: 'CR-V', value: 'CR-V' },
+        { key: '5', text: 'CR-Z', value: 'CR-Z' },
+        { key: '5', text: 'Freed', value: 'Freed' },
+        { key: '5', text: 'HR-V', value: 'HR-V' },
+        { key: '5', text: 'Insight', value: 'Insight' },
+        { key: '5', text: 'Integra', value: 'Integra' },
+        { key: '5', text: 'Jazz', value: 'Jazz' },
+        { key: '5', text: 'Legend', value: 'Legend' },
+        { key: '5', text: 'Mobilio', value: 'Mobilio' },
+        { key: '5', text: 'NSX', value: 'NSX' },
+        { key: '5', text: 'Odyssey', value: 'Odyssey' },
+        { key: '5', text: 'Prelude', value: 'Prelude' },
+        { key: '5', text: 'S2000', value: 'S2000' },
+        { key: '5', text: 'STEPWGN SPADA', value: 'STEPWGN SPADA' },
+        { key: '5', text: 'Stream', value: 'Stream' },
+        { key: '5', text: 'Tourmaster', value: 'Tourmaster' },
+        { key: '5', text: 'Vigor', value: 'Vigor' },
+    ]
+
+    const modelDetailOptions = [
+        {key: '', text: '', value: ''},
+        { key: '1', text: '2.0 E i-VTEC 4Doors', value: 'three' },
+        { key: '2', text: '2.0 EL i-VTEC 4Doors', value: 'two' },
+        { key: '3', text: '2.0 Hybrid i-VTEC 4Doors', value: 'one' },
+        { key: '3', text: '2.0 Hybrid TECH i-VTEC 4Doors', value: 'one' },
+        { key: '3', text: '2.4 EL i-VTEC 4Doors', value: 'one' },
+    ]
+    
+    const yearOptions = [
+        {key: '', text: '', value: ''},
+        { key: '1', text: '2019', value: 'three' },
+        { key: '2', text: '2018', value: 'two' },
+        { key: '3', text: '2017', value: 'one' },
+        { key: '3', text: '2016', value: 'one' },
+        { key: '3', text: '2015', value: 'one' },
+    ]
+
+    const genderOptions = [
+        {key: '', text: '', value: ''},
+        { key: '1', text: '2019', value: 'three' },
+        { key: '2', text: '2018', value: 'two' },
+        { key: '3', text: '2017', value: 'one' },
+        { key: '3', text: '2016', value: 'one' },
+        { key: '3', text: '2015', value: 'one' },
+    ]
+
 
     return (
         <div>
@@ -16,25 +73,25 @@ function BasicCarInformationForm() {
                     <Form.Group widths='4'>
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={brandOptions}
                             label='ยี่ห้อ'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={modelOptions}
                             label='รุ่น'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={modelDetailOptions}
                             label='รายละเอียดรุ่น'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={yearOptions}
                             label='ปี่ที่ผลิต'
                             fluid
                         />
@@ -42,13 +99,13 @@ function BasicCarInformationForm() {
                     <Form.Group widths='2'>
                         <Form.Field
                             control={Input}
-                            options={genderOptions}
+                            // options={genderOptions}
                             label='หมายเลขตัวถัง'
                             fluid
                         />
                         <Form.Field
                             control={Input}
-                            options={genderOptions}
+                            // options={genderOptions}
                             label='หมายเลขตัวเครื่อง'
                             fluid
                         />
@@ -61,25 +118,25 @@ function BasicCarInformationForm() {
                     <Form.Group widths='4'>
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={brandOptions}
                             label='ยี่ห้อ'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={modelOptions}
                             label='รุ่น'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={modelDetailOptions}
                             label='รายละเอียดรุ่น'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={yearOptions}
                             label='ปี่ที่ผลิต'
                             fluid
                         />
@@ -87,13 +144,13 @@ function BasicCarInformationForm() {
                     <Form.Group widths='2'>
                         <Form.Field
                             control={Input}
-                            options={genderOptions}
+                            // options={genderOptions}
                             label='หมายเลขตัวถัง'
                             fluid
                         />
                         <Form.Field
                             control={Input}
-                            options={genderOptions}
+                            // options={genderOptions}
                             label='หมายเลขตัวเครื่อง'
                             fluid
                         />
@@ -107,25 +164,25 @@ function BasicCarInformationForm() {
                     <Form.Group widths='4'>
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={brandOptions}
                             label='ยี่ห้อ'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={modelOptions}
                             label='รุ่น'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={modelDetailOptions}
                             label='รายละเอียดรุ่น'
                             fluid
                         />
                         <Form.Field
                             control={Select}
-                            options={genderOptions}
+                            options={yearOptions}
                             label='ปี่ที่ผลิต'
                             fluid
                         />
@@ -133,13 +190,13 @@ function BasicCarInformationForm() {
                     <Form.Group widths='2'>
                         <Form.Field
                             control={Input}
-                            options={genderOptions}
+                            // options={genderOptions}
                             label='หมายเลขตัวถัง'
                             fluid
                         />
                         <Form.Field
                             control={Input}
-                            options={genderOptions}
+                            // options={genderOptions}
                             label='หมายเลขตัวเครื่อง'
                             fluid
                         />
