0eb49671970f5d58ac7d2fdfdcf368d94aff3414
Kitpavin <Chaiwong>
2019-09-30 16:53:48 +0700

    stupid change

--- src/components/Home/InsuranceDetails.js
+++ src/components/Home/InsuranceDetails.js
@@ -4,6 +4,7 @@ import { Menu, Segment, Grid } from 'semantic-ui-react'
 function InsuranceDetails(props) {    
     const [selectedType, setSelectedType] = useState(props.insuranceType)
     const [activeItem, setActiveItem] = useState(selectedType+'-1')
+    const []
 
     useEffect(() => {
         if(selectedType !== props.insuranceType){
@@ -16,40 +17,55 @@ function InsuranceDetails(props) {
 
     const handleItemClick = (e, { name }) => setActiveItem(name)
 
-    return (
-      <Grid>
-        <Grid.Column width={4}>
-          <Menu fluid vertical tabular>
-            <Menu.Item
-              name={selectedType + '-1'}
-              active={activeItem === (selectedType + '-1')}
-              onClick={handleItemClick}
-            />
-            <Menu.Item
-              name={selectedType + '-2'}
-              active={activeItem === (selectedType + '-2')}
-              onClick={handleItemClick}
-            />
-            <Menu.Item
-              name={selectedType + '-3'}
-              active={activeItem === (selectedType + '-3')}
-              onClick={handleItemClick}
-            />
-            <Menu.Item
-              name={selectedType + '-4'}
-              active={activeItem === (selectedType + '-4')}
-              onClick={handleItemClick}
-            />
-          </Menu>
-        </Grid.Column>
 
-        <Grid.Column stretched width={12}>
-          <Segment>
-            This is an stretched grid column. This segment will always match the
-            tab height
-          </Segment>
-        </Grid.Column>
-      </Grid>
+    // selectType = () => {
+    //   if(this.props.insuranceType === 1) return list
+    //   else return listee
+    // }
+
+    var listee = <div>
+      listee
+    </div>
+    
+    var list =       
+              <Grid>
+                <Grid.Column width={4}>
+                  <Menu fluid vertical tabular>
+                    <Menu.Item
+                      name={selectedType + '-1'}
+                      active={activeItem === (selectedType + '-1')}
+                      onClick={handleItemClick}
+                    />
+                    <Menu.Item
+                      name={selectedType + '-2'}
+                      active={activeItem === (selectedType + '-2')}
+                      onClick={handleItemClick}
+                    />
+                    <Menu.Item
+                      name={selectedType + '-3'}
+                      active={activeItem === (selectedType + '-3')}
+                      onClick={handleItemClick}
+                    />
+                    <Menu.Item
+                      name={selectedType + '-4'}
+                      active={activeItem === (selectedType + '-4')}
+                      onClick={handleItemClick}
+                    />
+                    </Menu>
+                </Grid.Column>
+
+                <Grid.Column stretched width={12}>
+                  <Segment>
+                    This is an stretched grid column. This segment will always match the
+                    tab height
+                  </Segment>
+                </Grid.Column>
+              </Grid>
+
+    return (
+      <div>
+        {list}
+      </div>
     )
 }
 
